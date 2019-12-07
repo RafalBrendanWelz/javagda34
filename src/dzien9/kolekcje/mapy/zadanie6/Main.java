900ff0b25e2a9758095c172fd104b7452911c3ea
chodekm <chodekm@gmail.com>
2019-12-07 12:22:33 +0100

    Zdanie ze skorowidzem

---
+++ src/dzien9/kolekcje/mapy/zadanie6/Main.java
@@ -0,0 +1,11 @@
+package dzien9.kolekcje.mapy.zadanie6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String hello = "Ala ma kota";
+
+        System.out.println(Skorowidz.skorowidzLiterowy(hello));
+
+    }
+}
